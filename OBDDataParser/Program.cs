fe9be4c1446e3ced08fc892257c0173c84945d4a
Emanuele Anfuso <emanuele19@gmail.com>
2018-05-13 19:12:53 +0200

    Little changes to make things work easier.

--- OBDDataParser/Program.cs
+++ OBDDataParser/Program.cs
@@ -10,10 +10,25 @@ namespace OBDDataParser
     {
         static void Main(string[] args)
         {
-            OBDRawDataFormatter obdFormatter = new OBDRawDataFormatter();
+            string name;
+            CommandType commandType;
 
-            string name = "10-05-18_14-10-39_speed_measure";
-            CommandType commandType = CommandType.speed;
+            name = "10-05-18_21-04-46_speed_measure";
+            commandType = CommandType.speed;
+            ProcessRawData(name, commandType);
+
+            name = "10-05-18_21-04-46_rpm_measure";
+            commandType = CommandType.rpm;
+            ProcessRawData(name, commandType);
+        }
+
+        /// <summary>
+        /// It takes the nema of raw data files (in desktop folder) and save all the various
+        /// versions in other txt files. The most useful are the converted one.
+        /// </summary>
+        private static void ProcessRawData(string name, CommandType commandType)
+        {
+            OBDRawDataFormatter obdFormatter = new OBDRawDataFormatter();
 
             string path = "C:\\Users\\emanu\\Desktop\\" + name + ".txt";
             string formattedRawData = obdFormatter.ReadAndFormatRawData(path);
